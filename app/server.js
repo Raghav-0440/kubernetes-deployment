a68c610bfdf2e0f66f4b09a975616d8a81921451
Raghav Gupta <114011200+Raghav-0440@users.noreply.github.com>
2025-10-24 21:17:07 +0530

    changes

--- app/server.js
+++ app/server.js
@@ -4,5 +4,5 @@ const version = process.env.APP_VERSION || 'v1';
 
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type':'text/plain'});
-  res.end(`hello from ${version}\n`);
+  res.end(`Hello from Blue-Green Deployment (${version})\n`);
 }).listen(port, () => console.log('listening', port));
